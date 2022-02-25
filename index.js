f828f9e1bb8cc2dd5864479f67ad490ff0320bbe
marisasaunders <marisaksaunders@gmail.com>
2022-02-25 16:04:23 -0500

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
 class Formatter {
-  //add static methods here
+  static capitalize(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1)
+;  //takes in a string and capitalizes the first letter.
+  }
+
+  static sanitize(string){
+    return string.replace(/[^A-Za-z0-9-' ']/g, '');
+  //takes in a string and removes all non-alphanumeric characters except for dashes, single quotes and spaces
+  }
+
+  static titleize(string) {
+    console.log(string.split(' ').toUpperCase[0])
+  //takes in a string and capitalizes all words in a sentence except the, a, an, but, of, and, for, at, by, and from; and always capitalizes the first word
+  }
 }
