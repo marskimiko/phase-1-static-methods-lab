b7bf299b673020ad28af5ba30fc6bd68ff19a375
marisasaunders <marisaksaunders@gmail.com>
2022-02-25 16:21:21 -0500

    Done.

--- index.js
+++ index.js
@@ -10,9 +10,30 @@ class Formatter {
   }
 
   static titleize(string) {
-    let stringArray = string.split(' ')
-    let capFirst = stringArray[0].toUpperCase()
-    console.log(capFirst)
+    const words = string.toLowerCase().split(" ");
+  let newWords = words.map(word => {
+    if ( word !== 'the' && word !== 'a' && word !== 'an' && word !== 'but'  && word !== 'of' && word !== 'and' && word !== 'for' && word !== 'at' && word !== 'by' && word !== 'from'){
+      return word[0].toUpperCase() + word.slice(1)
+    } else {
+      return word
+    }
+  }).join(" ")
+  return newWords[0].toUpperCase() + newWords.slice(1)
+    // let stringArray = string.split(' ')
+    // let cap = stringArray.toUpperCase()
+    // console.log(cap)
   //takes in a string and capitalizes all words in a sentence except the, a, an, but, of, and, for, at, by, and from; and always capitalizes the first word
   }
+}
+
+// function titleizer(string){
+//   const words = string.toLowerCase().split(" ");
+//   let newWords = words.map(word => {
+//     if ( word !== 'the' && word !== 'a' && word !== 'an' && word !== 'but'  && word !== 'of' && word !== 'and' && word !== 'for' && word !== 'at' && word !== 'by' && word !== 'from'){
+//       return word[0].toUpperCase() + word.slice(1)
+//     } else {
+//       return word
+//     }
+//   }).join(" ")
+//   return newWords[0].toUpperCase() + newWords.slice(1)
 }
